cf179735dfc5faff8a19e501af10ae063845a777
ST10473765-AntonvanVondel <st10473765@myemeris.edu.za>
2026-03-27 22:13:32 +0200

    Initial Commit
    Committed a random project of an exercise, this is only to show that I know how to set up a repository as requested on the assignment on part 1

---
+++ DelegatesExercise/DelegatesExercise/Methods.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+delegate int MarkProcessor(int inp);
+namespace DelegatesExercise
+{
+    internal class Methods
+    {
+        public int AddBonus(int inp) { return inp + 5; }
+        public int CapMark(int inp) { if (AddBonus(inp) > 100) { return 100; } else { return AddBonus(inp); } }
+        public int PassStatus(int inp) { if (AddBonus(inp) >= 50) { return 1; } else { return 0; } }
+        public Methods()
+        {
+            Console.WriteLine("Welcome to the Exercise");
+        }
+
+        public void Output(int y)
+        {
+            Console.Write("Bonus added to mark: ");
+            ProcessMark(y, AddBonus);
+            Console.Write($"Mark Capped: ");
+            ProcessMark(y, CapMark);
+            Console.Write("Pass Status: ");
+            ProcessMark(y, PassStatus);
+        }
+        public void ProcessMark(int x, MarkProcessor operation)
+        {
+            if (operation == PassStatus)
+            {
+                if (operation(x) == 0)
+                {
+                    Console.WriteLine("Failed");
+                }
+                else if (operation(x) == 1)
+                {
+                    Console.WriteLine("Passed");
+                }
+            }
+            else
+            {
+                Console.WriteLine(operation(x));
+            }
+
+        }
+    }
+}
---
+++ DelegatesExercise/DelegatesExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DelegatesExercise
+{
+    internal class Program
+    {
+        static Methods methods = new Methods();
+        static void Main(string[] args)
+        {
+            Console.Write("Enter mark: ");
+            int mark = validInt(Console.ReadLine());
+            while (mark < 0)
+            {
+                Console.Write("Try again: ");
+                mark = validInt(Console.ReadLine());
+            }
+            methods.Output(mark);
+        }
+
+        static int validInt(string inp)
+        {
+            try
+            {
+                return int.Parse(inp);
+            } catch (FormatException)
+            {
+                Console.WriteLine("Invalid integer value");
+                return -1;
+            }
+        }
+    }
+}
